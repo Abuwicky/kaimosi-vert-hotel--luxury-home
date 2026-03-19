20c2dffccf198d727a90bf352a28d9229aa35bde
Wycliffe Aburiri <abuririwycliffe@gmail.com>
2026-03-19 18:22:34 +0300

    Updated supabase client configuration

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://hzsjuuxbxkrqbyhcasmi.supabase.co"; // From your Supabase project
-const supabaseAnonKey = "sb_publishable_xrXhcBYT86o23TyHJxcrcg_a5NQ3MeD"; // From your Supabase project
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6c2p1dXhieGtycWJ5aGNhc21pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5MTg2NDYsImV4cCI6MjA4OTQ5NDY0Nn0.E7wI7o43fvCB3EbWKE6Q-qYr7OKZLpdR4C-p1RdhAhY"; // From your Supabase project
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
